1ccbaf87125ffdadd137ca68ee9af7ce4fca9256
creativerze <creativerze@gmail.com>
2023-01-07 12:19:20 +0600

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Meta from "../components/Meta";
+
+const about = () => {
+  return (
+    <div>
+      <Meta title='About'></Meta>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
